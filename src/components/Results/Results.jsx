6878b95fb482590e870ef0d7a0169691cf9d6511
Dawa Sherpa <dasherpa7@gmail.com>
2017-01-14 22:17:53 -0500

    Update style and show more profiles

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -52,15 +52,92 @@ class Results extends Component {
             <a href={"http://facebook.com/" + this.state.channels[0].id}>Facebook: {this.state.channels[0].id}</a><br/>
             <a href={"http://twitter.com/" + this.state.channels[1].id}>@{this.state.channels[1].id}</a>
             <p>{this.state.address[0].line1}</p><p>{this.state.address[0].city}</p><p>{this.state.address[0].state}, {this.state.address[0].zup}</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ultrices ligula. Etiam neque dui, tincidunt nec ullamcorper in, lobortis in nisl. Morbi blandit semper risus, id finibus nisl laoreet a. Phasellus eget vestibulum magna, vitae aliquam neque. Phasellus suscipit quis massa in varius. Mauris id turpis in tortor faucibus efficitur non vel nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin ut rhoncus orci. Proin id condimentum ligula. Pellentesque placerat augue justo, et suscipit diam aliquet eget. Nulla eget odio ut ligula euismod tincidunt a non mauris. Aenean magna nisl, auctor vitae lorem at, ultrices efficitur eros. Ut tristique sem quis lorem ullamcorper facilisis. Aenean id leo augue.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ultrices ligula.</p>
 
           </aside>
 
         </div>
+        <div className='rep-cont'>
+          <img src={this.state.photoUrl} className='rep-img'/>
+          <aside className='rep-info'>
+            <h3>{this.state.name} ({this.state.party})</h3>
+            <a href={"http://facebook.com/" + this.state.channels[0].id}>Facebook: {this.state.channels[0].id}</a><br/>
+            <a href={"http://twitter.com/" + this.state.channels[1].id}>@{this.state.channels[1].id}</a>
+            <p>{this.state.address[0].line1}</p><p>{this.state.address[0].city}</p><p>{this.state.address[0].state}, {this.state.address[0].zup}</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ultrices ligula.</p>
+
+          </aside>
+
+        </div>
+        <div className='rep-cont'>
+          <img src={this.state.photoUrl} className='rep-img'/>
+          <aside className='rep-info'>
+            <h3>{this.state.name} ({this.state.party})</h3>
+            <a href={"http://facebook.com/" + this.state.channels[0].id}>Facebook: {this.state.channels[0].id}</a><br/>
+            <a href={"http://twitter.com/" + this.state.channels[1].id}>@{this.state.channels[1].id}</a>
+            <p>{this.state.address[0].line1}</p><p>{this.state.address[0].city}</p><p>{this.state.address[0].state}, {this.state.address[0].zup}</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ultrices ligula.</p>
+
+          </aside>
+
+        </div>
+        <div className='rep-cont'>
+          <img src={this.state.photoUrl} className='rep-img'/>
+          <aside className='rep-info'>
+            <h3>{this.state.name} ({this.state.party})</h3>
+            <a href={"http://facebook.com/" + this.state.channels[0].id}>Facebook: {this.state.channels[0].id}</a><br/>
+            <a href={"http://twitter.com/" + this.state.channels[1].id}>@{this.state.channels[1].id}</a>
+            <p>{this.state.address[0].line1}</p><p>{this.state.address[0].city}</p><p>{this.state.address[0].state}, {this.state.address[0].zup}</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ultrices ligula.</p>
+
+          </aside>
+
+        </div>
+
+        <div className='rep-cont'>
+          <img src={this.state.photoUrl} className='rep-img'/>
+          <aside className='rep-info'>
+            <h3>{this.state.name} ({this.state.party})</h3>
+            <a href={"http://facebook.com/" + this.state.channels[0].id}>Facebook: {this.state.channels[0].id}</a><br/>
+            <a href={"http://twitter.com/" + this.state.channels[1].id}>@{this.state.channels[1].id}</a>
+            <p>{this.state.address[0].line1}</p><p>{this.state.address[0].city}</p><p>{this.state.address[0].state}, {this.state.address[0].zup}</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ultrices ligula.</p>
+
+          </aside>
+
+        </div>
+
+
+        <div className='rep-cont'>
+          <img src={this.state.photoUrl} className='rep-img'/>
+          <aside className='rep-info'>
+            <h3>{this.state.name} ({this.state.party})</h3>
+            <a href={"http://facebook.com/" + this.state.channels[0].id}>Facebook: {this.state.channels[0].id}</a><br/>
+            <a href={"http://twitter.com/" + this.state.channels[1].id}>@{this.state.channels[1].id}</a>
+            <p>{this.state.address[0].line1}</p><p>{this.state.address[0].city}</p><p>{this.state.address[0].state}, {this.state.address[0].zup}</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ultrices ligula.</p>
+
+          </aside>
+
+        </div>
+
+
+        <div className='rep-cont'>
+          <img src={this.state.photoUrl} className='rep-img'/>
+          <aside className='rep-info'>
+            <h3>{this.state.name} ({this.state.party})</h3>
+            <a href={"http://facebook.com/" + this.state.channels[0].id}>Facebook: {this.state.channels[0].id}</a><br/>
+            <a href={"http://twitter.com/" + this.state.channels[1].id}>@{this.state.channels[1].id}</a>
+            <p>{this.state.address[0].line1}</p><p>{this.state.address[0].city}</p><p>{this.state.address[0].state}, {this.state.address[0].zup}</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ultrices ligula.</p>
+
+          </aside>
+
+        </div>
+
       </section>
     )
   }
 }
 
 export default Results;
-
