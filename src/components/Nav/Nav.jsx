cc739f275df74afae64c1e6fc3e558e247b927b7
Dawa Sherpa <dasherpa7@gmail.com>
2017-01-14 22:18:13 -0500

    Add Nav component

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link, browserHistory } from 'react-router';
+import styles from './Nav.css';
+
+class Nav extends Component {
+  render() {
+    return(
+      <header className='nav-header'>
+        <h1>RepDotBody</h1>
+
+      </header>
+    )
+  }
+}
+
+export default Nav;
