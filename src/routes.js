a84166ef63c10758e274fe98bc4ff79005c71e18
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2017-01-14 19:12:36 -0500

    added UpdateUser component

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Route, IndexRoute, Link } from 'react-router';
-import App from './components/App.jsx';
-import Home from './components/Home/Home.jsx';
-import LogIn from './components/LogIn/LogIn.jsx';
-import CreateUser from './components/CreateUser/CreateUser.jsx';
-import Results from './components/Results/Results.jsx';
-import Main from './components/Main/Main.jsx';
+import App from           './components/App.jsx';
+import Home from          './components/Home/Home.jsx';
+import LogIn from         './components/LogIn/LogIn.jsx';
+import CreateUser from    './components/CreateUser/CreateUser.jsx';
+import Results from       './components/Results/Results.jsx';
+import Main from          './components/Main/Main.jsx';
+import UpdateUser from    './components/UpdateUser/UpdateUser.jsx';
 
 
 module.exports = (
@@ -15,5 +16,6 @@ module.exports = (
     <Route path='/home' component={Home} />
     <Route path='/main' component={Main} />
     <Route path='/results' component={Results} />
+    <Route path='/update' component={UpdateUser} />
   </Route>
 );
